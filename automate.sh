2b037e1ae1642c32199ce319b325a4ba9835e270
RamonHA <ramon1001@live.com.mx>
2022-02-04 16:59:44 -0600

    Update Notion index.html version

---
+++ automate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check new file index.html
+
+# Remove previous index.html
+
+# Rename new index.html to index.html
+
+git add .
+git commit -m "Update Notion index.html version"
+git push
